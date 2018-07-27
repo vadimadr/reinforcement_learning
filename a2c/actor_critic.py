Fix cumulative return calculation for the last observation in a session

a2c/actor_critic.py
@@ -32,10 +32,10 @@ class ActorCritic(Agent):
         state_dict = checkpoint.get('state_dict', checkpoint)
         self.model.load_state_dict(state_dict)
 
-    def update(self, states, actions, rewards, dones=None):
+    def update(self, states, actions, cumulative_rewards, dones=None):
         states = torch.tensor(states).to(device, torch.float)
         actions = torch.tensor(actions).to(device, torch.float)
-        cumulative_returns = torch.tensor(get_cumulative_rewards(rewards, self.gamma, dones)).to(device)
+        cumulative_returns = torch.tensor(cumulative_rewards).to(device, torch.float)
 
         # predict logits, probas and log-probas using an agent.
         logits, values = self.model(states)
@@ -60,6 +60,11 @@ class ActorCritic(Agent):
             dist = Categorical(F.softmax(logits, dim=1))
             sample = dist.sample()
             return sample.detach().cpu().numpy()
+
+    def get_value(self, state):
+        with torch.no_grad():
+            _, values = self.model(torch.tensor(state).to(device, torch.float))
+            return values.detach().cpu().numpy()
 
 
 class MLPPolicy(nn.Module):
@@ -89,20 +94,26 @@ def to_one_hot(y, n_dims=None):
     return y_one_hot
 
 
-def train(agent, env, args):
+def train(agent, env, args, max_reward=200):
     total_rewars = []
     for i in range(args.num_steps):
-        states, actions, rewards, dones = generate_session_batch(agent, env, args.episode_length)
+        states, actions, rewards, dones, lastvalues = generate_session_batch(agent, env, args.episode_length)
+
+        # get cumulative rewards
+        # use fictive reward for handling unfinished sessions
+        rewards_ = np.c_[rewards, lastvalues]
+        dones_ = np.c_[dones, np.zeros(dones.shape[0])]
+        cumulative_rewards = get_cumulative_rewards(rewards_, agent.gamma, dones=dones_)[:, :-1]
 
         # reshape sessions to form a batch
         batch_size = env.num_envs * args.episode_length
         states = states.reshape((batch_size,) + env.observation_space.shape)
         actions = actions.reshape((batch_size,))
-        rewards = rewards.reshape((batch_size,))
+        cumulative_rewards = cumulative_rewards.reshape((batch_size,))
         dones = dones.reshape((batch_size,))
 
         # update policy on a batch
-        losses = agent.update(states, actions, rewards, dones)
+        losses = agent.update(states, actions, cumulative_rewards, dones)
         # note that last session reward is truncated
         total_rewars.extend(get_total_rewards(rewards, dones))
 

utils.py
@@ -8,25 +8,34 @@ def get_cumulative_rewards(rewards, gamma=0.99, dones=None):
     compute cumulative returns (a.k.a. G(s,a) in Sutton '16)
     G_t = r_t + gamma*r_{t+1} + gamma^2*r_{t+2} + ...
     """
+    rewards = np.asarray(rewards)
     if dones is None:
-        dones = [False] * len(rewards)
+        dones = np.zeros(rewards.shape[-1])
+    dones = np.asarray(dones)
 
+    batch_size = rewards.shape[0] if len(rewards.shape) > 1 else 1
     cumulative_rewards = []
-    current_reward = 0
-    for r, done in zip(rewards[::-1], dones[::-1]):
-        # do mot discount if it is the last observation in a session
+    current_reward = np.zeros(batch_size)
+    for r, done in zip(rewards.T[::-1], dones.T[::-1]):
+        # do not discount if it is the last observation in a session
         current_reward = r + gamma * current_reward * (1 - done)
         cumulative_rewards.append(current_reward)
-    return cumulative_rewards[::-1]
+
+    return np.stack(cumulative_rewards[::-1], axis=1).reshape(rewards.shape)
 
 
 def get_total_rewards(rewards, dones):
     """
     Calculate total reward for all sessions in a batch
     """
+    rewards = np.asarray(rewards)
+    dones = np.array(dones)
+    dones = dones.reshape((-1, dones.shape[-1]))
+    dones[:, -1] = False
+
     total_rewards = []
     current_total_reward = 0
-    for reward, done in zip(rewards, dones):
+    for reward, done in zip(rewards.flat, dones.flat):
         current_total_reward += reward
         if done:
             total_rewards.append(current_total_reward)
@@ -82,9 +91,10 @@ def generate_session_batch(agent, envs, num_steps=32):
     actions = np.asarray(actions, dtype=np.int32).swapaxes(0, 1)
     rewards = np.asarray(rewards, dtype=np.float32).swapaxes(0, 1)
     dones = np.asarray(dones, dtype=bool).swapaxes(0, 1)
-    dones[:, -1] = True
 
-    return states, actions, rewards, dones
+    last_value = agent.get_value(s)
+
+    return states, actions, rewards, dones, last_value
 
 
 def make_fun(agent, env, record_video=False, render=True, n_episodes=1):